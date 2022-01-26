45b6dd9e1400b4c9f5656c43b44c6cae102d30d3
josecorredor <jose.corredor@linqueate.com>
2022-01-26 19:27:01 +1000

    JoseUpdate

--- src/app.ts
+++ src/app.ts
@@ -18,8 +18,8 @@ createConnection().then(connection => {
   };
 
   routes(app);
-  const port = process.env.PORT || 5000;
+  const port = process.env.PORT || 8000;
   app.listen(port, () => {
-      console.log('Listening to port 5000')
+      console.log('Listening to port 8000')
   });
 });
