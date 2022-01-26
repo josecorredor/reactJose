7fdb72cee3359b4128b837c5be940ce8b428ce21
josecorredor <jose.corredor@linqueate.com>
2022-01-26 19:00:14 +1000

    joseUpdate

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ createConnection().then(connection => {
   };
 
   routes(app);
-  const port = process.env.port || 8081;
+  const port = process.env.PORT || 8081;
   app.listen(port, () => {
       console.log('Listening to port 8081')
   });
